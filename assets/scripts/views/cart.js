9a2541bbf880533d76ef42d91d09dd0eb4d06f26
Dark_Cs <darkcs2@gmail.com>
2015-09-07 19:05:02 +0300

    Update 2015-09-07T16:04:58.934Z

--- assets/scripts/views/cart.js
+++ assets/scripts/views/cart.js
@@ -1,9 +1,57 @@
 $(function () {
     'use strict';
 
+    var PRICE_SPACER = ' ';
+
+    function formatPrice(x) {
+        var s = x.toString(),
+            part = '',
+            parts = [];
+
+        while (part = s.slice(-3)) {
+            parts.unshift(part);
+            s = s.slice(0, -3);
+        }
+
+        return parts.join(PRICE_SPACER);
+    }
+
+
+    function recalculateCart(el) {
+        var items = $('.js-cart__item', el),
+            totalEl = $('.js-cart__total', el),
+            subtotalEl = $('.js-cart__subtotal', el),
+            totalOldEl = $('.js-cart__total-old', el),
+            countEl = $('.js-cart__items-count', el),
+            deliveryValue = Number($('input.js-cart__delivery', el).val()) || 0,
+            totalOldValue = 0,
+            totalValue = 0,
+            subtotalValue = 0;
+
+        items.each(function (i, el) {
+            var $price = $('.js-cart__item-price', el),
+                $quantity = $('.js-cart__item-quantity', el);
+
+            subtotalValue += $price.val() * $quantity.val();
+
+        });
+
+        if (countEl.length) {
+            countEl.html(items.length);
+        }
+        if (subtotalEl.length) {
+            subtotalEl.html(formatPrice(subtotalValue));
+        }
+
+    }
+
     $.fn.cart = function () {
         this.off('change.cart');
         this.off('click.cart');
+
+        this.on('change.cart', function () {
+            recalculateCart(this);
+        });
 
         this.delegate('.js-cart__item', 'click.cart', function (event) {
             var el = this,
